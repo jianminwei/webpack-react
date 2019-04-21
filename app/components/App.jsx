020ae44b5baba2c01469ec2a7a3dbc93667d1e0d
Jianmin <jianminwei1@gmail.com>
2019-04-21 09:05:28 -0400

    created Note.jsx and Notes.jsx components

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,6 +1,6 @@
 import uuid from 'node-uuid';
 import React from 'react';
-//import Note from './Note.jsx';
+import Notes from './Notes.jsx';
 export default class App extends React.Component {
     constructor(props) {
         super(props);
@@ -23,17 +23,12 @@ export default class App extends React.Component {
         };
     }
     render() {
-
         const notes = this.state.notes;
 
         return (
-            
             <div>
                 <button onClick={this.addNote}>+</button>
-
-                <ul>{notes.map(note =>
-                    <li key={note.id}>{note.task}</li>
-                )}</ul>
+                <Notes notes={notes} />
             </div>
         );
     }
